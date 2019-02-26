b356f7d9f6d7593b3e31b5e5c17473775540ab3b
Richard Parkins <rgparkins@hotmail.com>
2019-02-26 09:44:18 +0000

    Add DNS helper script

--- 05-create-elb.sh
+++ 05-create-elb.sh
@@ -3,10 +3,11 @@
 CONFIG=$(<configuration.json)
 
 SUBNET_ID=`echo $CONFIG | jq '."subnet-id"' | tr -d '"'`
-SG_NAME=elastic-search-elb
+SG_NAME=`echo $CONFIG | jq '."security-group-name-for-elb"' | tr -d '"'`
 SECURITY_GROUPS=`echo $(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME})`
 VPC_ID=`echo $CONFIG | jq '."vpc-id"' | tr -d '"'`
 MY_IP=`echo $(curl -s http://whatismyip.akamai.com/)`
+NAME=`echo $CONFIG | jq '."name"' | tr -d '"'`
 
 if [ `echo $SECURITY_GROUPS | jq '.SecurityGroups | length'` -eq 1 ]
 then
@@ -16,22 +17,22 @@ then
 else
   echo "Group does not exist.. creating"
 
-  TMP=`echo $(aws ec2 create-security-group --description 'elastic search development' --group-name=${SG_NAME} --vpc-id ${VPC_ID})`
+  TMP=`echo $(aws ec2 create-security-group --description '${SG_NAME}' --group-name=${SG_NAME} --vpc-id ${VPC_ID})`
 
   SECURITY_GROUP_ID=`echo $TMP | jq '.GroupId' | tr -d '"'`
 
-  aws ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --ip-permissions IpProtocol=tcp,FromPort=5601,ToPort=5601,IpRanges="[{CidrIp=${MY_IP},Description='node communication within the VPC'}]"
+  aws ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --ip-permissions IpProtocol=tcp,FromPort=5601,ToPort=5601,IpRanges="[{CidrIp=${MY_IP}/32,Description='node communication within the VPC'}]"
 
   echo "Group created"
 fi
 
-DNS=`echo $(aws elb create-load-balancer --load-balancer-name elasticsearch-test \
+DNS=`echo $(aws elb create-load-balancer --load-balancer-name ${NAME} \
   --security-groups ${SECURITY_GROUP_ID} \
   --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=5601" \
   --subnets ${SUBNET_ID})`
 
-IDS=`echo $(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:Name,Values=elasticsearch-test-master" --query "Reservations[].Instances[].InstanceId" --output text)`
+IDS=`echo $(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:Name,Values=${NAME}-master" --query "Reservations[].Instances[].InstanceId" --output text)`
 
-aws elb register-instances-with-load-balancer --load-balancer-name elasticsearch-test --instances ${IDS}
+aws elb register-instances-with-load-balancer --load-balancer-name ${NAME} --instances ${IDS}
 
 echo $DNS
---
+++ 06-create-dns.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+CONFIG=$(<configuration.json)
+
+SUBNET_ID=`echo $CONFIG | jq '."subnet-id"' | tr -d '"'`
+SG_NAME=`echo $CONFIG | jq '."security-group-name-for-elb"' | tr -d '"'`
+SECURITY_GROUPS=`echo $(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME})`
+VPC_ID=`echo $CONFIG | jq '."vpc-id"' | tr -d '"'`
+MY_IP=`echo $(curl -s http://whatismyip.akamai.com/)`
+NAME=`echo $CONFIG | jq '."name"' | tr -d '"'`
+
+ELB=`aws elb describe-load-balancers --load-balancer-names "quoting-elasticsearch-nonprod"`
+
+ELBDNSNAME=`echo $ELB | jq '.LoadBalancerDescriptions[0].CanonicalHostedZoneName' | tr -d '"'`
+HOSTEDZONEID=`echo $CONFIG | jq '."hosted-zone-id"' | tr -d '"'`
+DNS=`echo $CONFIG | jq '."dns"' | tr -d '"'`
+
+echo $DNSNAME
+echo $DNS
+
+sed  -e "s/DNS/${DNS}/g" -e "s/ELB/${ELBDNSNAME}/g" ./06-change_record_set.json | tee ./change_record_set.json
+
+aws route53 change-resource-record-sets --hosted-zone-id "${HOSTEDZONEID}" --change-batch "file://change_record_set.json"
