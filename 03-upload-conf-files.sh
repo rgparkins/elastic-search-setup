450ffb505b9f266747dc5159b0215bf4243faef2
Richard Parkins <rgparkins@hotmail.com>
2019-03-11 11:26:19 +0000

    simpler use of start up nodes. ebs volumes now created automatically via run-instances

--- 03-upload-conf-files.sh
+++ 03-upload-conf-files.sh
@@ -9,7 +9,6 @@ REGION=`echo ${CONFIG} | jq '."region"' | tr -d '"'`
 
 sed  -e "s/my-subnet/${REGION}/g" -e "s/tag-cluster/${NAME}/g" -e "s/my_cluster_name/${NAME}/g" ./03-elasticsearch_data.yml | tee ./elasticsearch_data.yml
 sed  -e "s/my-subnet/${REGION}/g" -e "s/tag-cluster/${NAME}/g" -e "s/my_cluster_name/${NAME}/g" ./03-elasticsearch_master.yml | tee ./elasticsearch_master.yml
-sed  -e "s/my-subnet/${REGION}/g" -e "s/tag-cluster/${NAME}/g" -e "s/my_cluster_name/${NAME}/g" ./03-elasticsearch_client.yml | tee ./elasticsearch_client.yml
 sed  "s/my-region/${REGION}/g" ./03-Dockerfile | tee ./Dockerfile
 
 echo "Pushing up common files"
@@ -25,9 +24,5 @@ aws s3 cp ./03-docker-compose.data.yml s3://${BUCKET_NAME}/elasticsearch/docker-
 aws s3 cp ./elasticsearch_master.yml s3://${BUCKET_NAME}/elasticsearch/elasticsearch_master.yml --region ${REGION}
 aws s3 cp ./03-master.jvm.options s3://${BUCKET_NAME}/elasticsearch/master.jvm.options --region ${REGION}
 aws s3 cp ./03-docker-compose.master.yml s3://${BUCKET_NAME}/elasticsearch/docker-compose.master.yml --region ${REGION}
-
-aws s3 cp ./elasticsearch_client.yml s3://${BUCKET_NAME}/elasticsearch/elasticsearch_client.yml --region ${REGION}
-aws s3 cp ./03-client.jvm.options s3://${BUCKET_NAME}/elasticsearch/client.jvm.options --region ${REGION}
-aws s3 cp ./03-docker-compose.client.yml s3://${BUCKET_NAME}/elasticsearch/docker-compose.client.yml --region ${REGION}
 
 echo "Files copied"
--- 04-start_nodes.sh
+++ 04-start_nodes.sh
@@ -11,14 +11,13 @@ AVAILABILITY_ZONE=`echo $CONFIG | jq '."availability-zone"' | tr -d '"'`
 REGION=`echo $CONFIG | jq '."region"' | tr -d '"'`
 MASTER_NODE_COUNT=`echo $CONFIG | jq '."master-node-count"' | tr -d '"'`
 DATA_NODE_COUNT=`echo $CONFIG | jq '."data-node-count"' | tr -d '"'`
-CLIENT_NODE_COUNT=`echo $CONFIG | jq '."client-node-count"' | tr -d '"'`
 NAME=`echo $CONFIG | jq '."name"' | tr -d '"'`
 IMAGE_NAME=`echo $CONFIG | jq '."target-image-name"' | tr -d '"'`
 TAGS=`echo $CONFIG | jq '."tags"' | tr -d '"'`
 
+
 sed  -e "s/my-region/${REGION}/g" -e "s/my-bucket/${BUCKET_NAME}/g" -e "s/my_cluster_name/${NAME}/g" ./04-runtime.data.userdata.txt | tee ./runtime.data.userdata.txt
 sed  -e "s/my-region/${REGION}/g" -e "s/my-bucket/${BUCKET_NAME}/g" -e "s/my_cluster_name/${NAME}/g" ./04-runtime.master.userdata.txt | tee ./runtime.master.userdata.txt
-sed  -e "s/my-region/${REGION}/g" -e "s/my-bucket/${BUCKET_NAME}/g" -e "s/my_cluster_name/${NAME}/g" ./04-runtime.client.userdata.txt | tee ./runtime.client.userdata.txt
 
 SECURITY_GROUPS=`echo $(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME})`
 
@@ -30,94 +29,52 @@ BASE_IMAGE_ID=`echo $AMIS | jq '.Images[0].ImageId' | tr -d '"'`
 
 echo "Using base id ${BASE_IMAGE_ID}"
 
-INSTANCES=`echo $(aws ec2 run-instances --image-id ${BASE_IMAGE_ID} \
-  --count ${MASTER_NODE_COUNT} \
-  --instance-type t2.xlarge \
-  --key-name ${KEY_VALUE_PAIR} \
-  --subnet-id ${SUBNET_ID} \
-  --iam-instance-profile Name=${ROLE_NAME} \
-  --user-data file://runtime.master.userdata.txt \
-  --tag-specifications="ResourceType=instance,Tags=[${TAGS},{Key=Name,Value=${NAME}-master}]" \
-  --associate-public-ip-address \
-  --security-group-ids ${SECURITY_GROUP_ID} | \
-jq -rc '.Instances[].InstanceId')`
+echo "Creating the data instances"
 
-echo $INSTANCES
+COUNTER=0
+while [ $COUNTER -lt ${DATA_NODE_COUNT} ]; do
+    let INDEX=$((COUNTER+1))
 
-echo "Waiting for instances to run state"
+    INSTANCES=`echo $(aws ec2 run-instances --image-id ${BASE_IMAGE_ID} \
+      --count 1 \
+      --instance-type t2.xlarge \
+      --key-name ${KEY_VALUE_PAIR} \
+      --subnet-id ${SUBNET_ID} \
+      --block-device-mappings file://04-data-mapping.json  \
+      --iam-instance-profile Name=${ROLE_NAME} \
+      --user-data file://runtime.data.userdata.txt \
+      --tag-specifications="ResourceType=instance,Tags=[${TAGS},{Key=Name,Value=${NAME}-data},{Key=Index,Value=${INDEX}}]" \
+      --associate-public-ip-address \
+      --security-group-ids ${SECURITY_GROUP_ID} | \
+    jq -rc '.Instances[].InstanceId')`
 
-aws ec2 wait instance-running --instance-ids $INSTANCES
+    let COUNTER=COUNTER+1
+done
 
 echo "instances now running"
 
-for instance in $INSTANCES; do
-   VOLUMEDATA=$(aws ec2 describe-volumes --filters Name=tag:Name,Values=${NAME}-${COUNT}-master --query "Volumes[*].{ID:VolumeId,Tag:Tags}")
+exit
 
-   if [ `echo $VOLUMEDATA | jq length` -eq 1 ]
-   then
-     VOLUMEID=`echo $VOLUMEDATA | jq .[].ID | tr -d '"'`
-     echo "volume already exists!"
-   else
-     echo "Creating volume"
-     VOLUMEID=`echo $(aws ec2 create-volume --availability-zone ${AVAILABILITY_ZONE} \
-       --volume-type gp2 --size 128 \
-       --tag-specifications "ResourceType=volume,Tags=[${TAGS},{Key=Name,Value=${NAME}-${COUNT}-master}]") | jq .VolumeId | tr -d '"'`
+echo "Creating the master instances"
 
-      aws ec2 wait volume-available --volume-ids $VOLUMEID
+COUNTER=0
+while [ $COUNTER -lt ${MASTER_NODE_COUNT} ]; do
+    let INDEX=$((COUNTER+1))
 
-      echo "Volume created"
-   fi
+   INSTANCES=`echo $(aws ec2 run-instances --image-id ${BASE_IMAGE_ID} \
+      --count 1 \
+      --instance-type t2.xlarge \
+      --key-name ${KEY_VALUE_PAIR} \
+      --subnet-id ${SUBNET_ID} \
+      --block-device-mappings file://04-master-mapping.json  \
+      --iam-instance-profile Name=${ROLE_NAME} \
+      --user-data file://runtime.master.userdata.txt \
+      --tag-specifications="ResourceType=instance,Tags=[${TAGS},{Key=Name,Value=${NAME}-master},{Key=Index,Value=${INDEX}}]" \
+      --associate-public-ip-address \
+      --security-group-ids ${SECURITY_GROUP_ID} | \
+    jq -rc '.Instances[].InstanceId')`
 
-   aws ec2 attach-volume --device /dev/xvdba --instance-id $instance --volume-id $VOLUMEID
-
-   aws ec2 modify-instance-attribute --instance-id $instance --block-device-mappings "[{\"DeviceName\": \"/dev/xvdba\",\"Ebs\":{\"DeleteOnTermination\":false}}]"
-
-   COUNT=$((COUNT+1))
+    let COUNTER=COUNTER+1
 done
 
-INSTANCES=`echo $(aws ec2 run-instances --image-id ${BASE_IMAGE_ID} \
-  --count ${DATA_NODE_COUNT} \
-  --instance-type t2.xlarge \
-  --key-name ${KEY_VALUE_PAIR} \
-  --subnet-id ${SUBNET_ID} \
-  --iam-instance-profile Name=${ROLE_NAME} \
-  --user-data file://runtime.data.userdata.txt \
-  --tag-specifications="ResourceType=instance,Tags=[${TAGS},{Key=Name,Value=${NAME}-data}]" \
-  --associate-public-ip-address \
-  --security-group-ids ${SECURITY_GROUP_ID} | \
-jq -rc '.Instances[].InstanceId')`
-
-echo "Waiting for instances to run state"
-
-aws ec2 wait instance-running --instance-ids $INSTANCES
-
-echo "instances now running"
-
-echo "Instance ids: ${INSTANCES}"
-
-COUNT=1
-for instance in $INSTANCES; do
-   VOLUMEDATA=$(aws ec2 describe-volumes --filters Name=tag:Index,Values=${COUNT}-data --query "Volumes[*].{ID:VolumeId,Tag:Tags}")
-
-   if [ `echo $VOLUMEDATA | jq length` -eq 1 ]
-   then
-     VOLUMEID=`echo $VOLUMEDATA | jq .[].ID | tr -d '"'`
-     echo "volume already exists!"
-   else
-     echo "Creating volume"
-     VOLUMEID=`echo $(aws ec2 create-volume --availability-zone ${AVAILABILITY_ZONE} \
-       --volume-type gp2 --size 1024 \
-       --tag-specifications "ResourceType=volume,Tags=[${TAGS},{Key=Index,Value=${COUNT}},{Key=Name,Value=${NAME}-${COUNT}-data}]") | jq .VolumeId | tr -d '"'`
-
-      aws ec2 wait volume-available --volume-ids $VOLUMEID
-
-      echo "Volume created"
-   fi
-
-   echo "instance id : ${instance} being attached to ${VOLUMEID}"
-   aws ec2 attach-volume --device /dev/xvdba --instance-id ${instance} --volume-id ${VOLUMEID}
-
-   aws ec2 modify-instance-attribute --instance-id $instance --block-device-mappings "[{\"DeviceName\": \"/dev/xvdba\",\"Ebs\":{\"DeleteOnTermination\":false}}]"
-
-   COUNT=$((COUNT+1))
-done
+echo "Finished creating the instances for master"
